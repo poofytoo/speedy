917f41c92ac99c15a994eedb90928b7f3462fed8
Stephanie Yu <styu314@gmail.com>
2016-06-19 00:56:01 -0400

    Scores endpoint and ignore config

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var auth = require('../auth');
+var firebaseRef = require('../firebase');
 
 /* GET home page. */
 router.get('/', auth.loggedIn, function(req, res, next) {
@@ -9,6 +10,23 @@ router.get('/', auth.loggedIn, function(req, res, next) {
 
 router.get('/login', function(req, res, next) {
   res.render('login', {});
-})
+});
+
+router.post('/score', auth.loggedIn, function(req, res, next) {
+  var scoreRef = firebaseRef.ref('scores');
+  scoreRef.push({
+    user: req.user,
+    score: req.body.score
+  });
+
+  res.send({success: true});
+});
+
+router.get('/scores', function(req, res, next) {
+  var scoreRef = firebaseRef.ref('scores');
+  scoreRef.orderByChild("score").on("value", function(snapshot) {
+    res.send({scores: snapshot.val()});
+  });
+});
 
 module.exports = router;
